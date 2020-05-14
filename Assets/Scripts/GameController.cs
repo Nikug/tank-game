9d64b73d4ff3b7292e9e7f75e50bb7baf539eec6
nikugronberg <niku.gronberg@student.lut.fi>
2020-05-14 11:58:58 +0300

    init

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Spawner spawner;
+
+    public float score;
+    public float scorePerTank;
+    public int lives;
+    public int enemyStartingAmount;
+    public int maxEnemiesAmount;
+
+    private int currentLives;
+    private int currentEnemyAmount;
+
+    private GameObject player;
+    public static GameController instance;
+    public UIController ui;
+    public AudioSource audioSource;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < enemyStartingAmount; i++)
+        {
+            spawner.SpawnEnemy();
+        }
+
+        player = spawner.SpawnPlayer();
+
+        score = 0f;
+        currentLives = lives;
+        currentEnemyAmount = enemyStartingAmount;
+
+        ui.SetScore(score);
+        ui.SetLives(currentLives, lives);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player == null)
+        {
+            if(currentLives > 0)
+            {
+                ui.ShowRespawnScreen();
+                if(Input.GetButtonDown("Restart"))
+                {
+                    audioSource.Play();
+                    player = spawner.SpawnPlayer();
+                    currentLives--;
+                    ui.SetLives(currentLives, lives);
+                    ui.HideRespawnScreen();
+                }
+            }
+            else
+            {
+                ui.ShowEndScreen(score);
+            }
+        }
+    }
+
+    public void EnemyDestroyed()
+    {
+        spawner.SpawnEnemy();
+        score += scorePerTank;
+        ui.SetScore(score);
+        if(currentEnemyAmount < maxEnemiesAmount)
+        {
+            spawner.SpawnEnemy();
+            currentEnemyAmount++;
+        }
+    }
+
+    public void SetHealth(float current, float maxHealth)
+    {
+        if(current < 0)
+        {
+            current = 0f;
+        }
+        ui.SetHealth(current, maxHealth);
+    }
+}
